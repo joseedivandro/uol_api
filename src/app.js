1b894d225c94fe565517377c1ea052d26621315b
joseedivandro <jose.edivandro.ja@outlook.com>
2023-04-13 17:34:12 -0300

    configurando banco e servidor

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import { MongoClient } from "mongodb"
+
+
+const PORT = 5000
+const app = express()
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => {
+    const mongoClient = new MongoClient(process.env.DATABASE_URL);
+    mongoClient.connect()
+        .then(() => {
+            const db = mongoClient.db();
+            console.log("MongoDB connected");
+            // configure your API routes and other middleware here
+        })
+        .catch((err) => {
+            console.error("Failed to connect to MongoDB", err);
+            process.exit(1);
+        });  
+    console.log(`Initialized server: port ${PORT}`);
+});
