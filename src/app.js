e25007e5fed731daec8e4d7f000a037c3ed8242a
joseedivandro <jose.edivandro.ja@outlook.com>
2023-04-14 17:11:40 -0300

    primeira tentativa do post

--- src/app.js
+++ src/app.js
@@ -23,6 +23,35 @@ app.listen(PORT, () => {
         .catch((err) => {
             console.error("Failed to connect to MongoDB", err);
             process.exit(1);
-        });  
+        });
     console.log(`Initialized server: port ${PORT}`);
 });
+
+app.post('./partipants', async (req, res) => {
+    try {
+        const participant = await participantRules.validateAsync(req.body)
+
+        const partipanteLocalizar = await db.collection("participants").findOne(participant)
+
+        if (partipanteLocalizar) return res.sendStatus(409)
+
+        await db.collection("participants").insertOne({ ...participant, lastStatus: Date.now() })
+
+        await db.collection("messages").insertOne({
+            from: participant.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        })
+
+        return res.sendStatus(201)
+
+    } catch (err) {
+        console.log(err)
+
+        if (err.isJoi) return res.sendStatus(400)
+
+        return res.sendStatus(500)
+    }
+}) 
